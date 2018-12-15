9a557a68eb8d9b67feefa6f3f167e5a64c186b37
Glitchy Scripts <killer_chief@hotmail.com>
2018-12-15 20:31:01 +1000

    Added product information and updated gitignore.

---
+++ xlivelessness/resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by xlive.rc
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
+
+#define DLL_VERSION_MAJOR       1
+#define DLL_VERSION_MINOR       0
+#define DLL_VERSION_REVISION    0
+#define DLL_VERSION_BUILD       1
+
+#define DLL_VERSION         DLL_VERSION_MAJOR, DLL_VERSION_MINOR, DLL_VERSION_REVISION, DLL_VERSION_BUILD
+#define STRINGIZE2(s)       #s
+#define STRINGIZE(s)        STRINGIZE2(s)
+#define DLL_VERSION_STR     STRINGIZE(DLL_VERSION_MAJOR) "." STRINGIZE(DLL_VERSION_MINOR) "." STRINGIZE(DLL_VERSION_REVISION) "." STRINGIZE(DLL_VERSION_BUILD)
